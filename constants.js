06d57a83a561af6e069150d27255d4105eac01db
Evgenii Grushin <evgenii.grushin@fortify.pro>
2022-04-10 22:41:44 -0700

    add

--- constants.js
+++ constants.js
@@ -1,16 +1,5 @@
 'use strict';
-
-// const FILE_SENTENCES_PATH = `./data/sentences.txt`;
-// const FILE_TITLES_PATH = `./data/titles.txt`;
-// const FILE_CATEGORIES_PATH = `./data/categories.txt`;
-// const FILE_COMMENTS_PATH = `./data/comments.txt`;
-const DEFAULT_PORT = 4000;
-// const FILENAME = `mocks.json`;
-// const MAX_COMMENTS = 4;
-
-// const MAX_ID_LENGTH = 6;
-// const DEFAULT_COUNT = 1;
-// const FILE_NAME = `mocks.json`;
+const DEFAULT_PORT = 8000;
 const DEFAULT_COMMAND = `--server`;
 const USER_ARGV_INDEX = 2;
 const ExitCode = {
@@ -18,10 +7,6 @@ const ExitCode = {
     error: 1
 };
 
-// const SumRestrict = {
-//   MIN: 1618066947,
-//   MAX: 1623337347,
-// };
 const HttpCode = {
     OK: 200,
     CREATED: 201,
@@ -35,20 +20,10 @@ const HttpCode = {
 const API_PREFIX = `/api`;
 
 module.exports = {
-    //   DEFAULT_COUNT,
-      DEFAULT_COMMAND,
-    //   FILE_NAME,
-    //   FILE_SENTENCES_PATH,
-    //   FILE_TITLES_PATH,
-    //   FILE_CATEGORIES_PATH,
-    //   FILE_COMMENTS_PATH,
-    //   SumRestrict,
+    DEFAULT_COMMAND,
     USER_ARGV_INDEX,
     ExitCode,
-      DEFAULT_PORT,
-    //   FILENAME,
-    //   MAX_ID_LENGTH,
-    //   MAX_COMMENTS,
+    DEFAULT_PORT,
     HttpCode,
     API_PREFIX
 };
