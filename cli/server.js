061487c324e9ae6a9b88e1e6dbaf391ee3422e97
Evgenii Grushin <evgenii.grushin@fortify.pro>
2022-04-14 04:07:28 -0700

    add

--- cli/server.js
+++ cli/server.js
@@ -29,6 +29,12 @@ app.use((req, res, next) => {
 
 app.use(API_PREFIX, routes);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use((req, res) => {
     res.status(HttpCode.NOT_FOUND)
         .send(`Not found`);
