f1ac32d5768e20adb6748037e892885cd14c0806
Evgenii Grushin <evgenii.grushin@fortify.pro>
2022-04-14 16:35:02 -0700

    add

--- api/user.js
+++ api/user.js
@@ -21,6 +21,12 @@ const ErrorAuthMessage = {
 
 module.exports = (app, service) => {
   app.use(`/user`, route);
+  app.options('/login', function (req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.end();
+  });
 
   route.get(`/`, async (req, res) => {
     const users = await service.findAll();
